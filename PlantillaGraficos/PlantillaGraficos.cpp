437f5ad60a344e5b647f0d1d722b7b6fd7d22561
alumno <alumno@E303>
2020-01-27 07:47:51 -0700

    Avance

--- PlantillaGraficos/PlantillaGraficos.cpp
+++ PlantillaGraficos/PlantillaGraficos.cpp
@@ -55,7 +55,6 @@ void dibujaVentana() {
 	glVertex3f(0.6f, 0.3f, 0.0f);
 	glVertex3f(0.30f, 0.3f, 0.0f);
 	glVertex3f(0.30f, 0.0f, 0.0f);
-
 	glEnd();
 
 }
@@ -91,18 +90,59 @@ void dibujarCesped() {
 
 }
 void dibujarPuerta() {
-	glBegin(GL_POLYGON);
+	glBegin(GL_TRIANGLE_STRIP);
 	glColor3f(0.0f, 0.5f, 0.5f);
 
-	glVertex3f(-0.2f, 0.0f, 0.0f);
-	glVertex3f(-0.1f, 0.2f, 0.0f);
-	glVertex3f(0.0f, 0.4f, 0.0f);
-	glVertex3f(0.1f, 0.2f, 0.0f);
-	glVertex3f(0.2f, 0.0f, 0.0f);
-	glVertex3f(0.1f, -0.2f, 0.0f);
-	glVertex3f(0.0f, -0.4f, 0.0f);
-	glVertex3f(-0.1f, -0.2f, 0.0f);
-	glVertex3f(-0.2f, 0.0f, 0.0f);
+	glVertex3f(0.1f, -0.5f, 0.0f);
+	glVertex3f(0.4f, -0.5f, 0.0f);
+	glVertex3f(0.4f, -0.1f, 0.0f);
+	glVertex3f(0.1f, -0.1f, 0.0f);
+	glVertex3f(0.1f, -0.5f, 0.0f);
+	glEnd();
+}
+void dibujarPomo() {
+	glBegin(GL_POLYGON);
+	glColor3f(1.0f, 0.96f, 0.21f);
+	for (double i = 0; i < 360; i += 0.5) {
+		glVertex3f(0.01 * cos(i * 3.1416 / 180) + 0.35, 0.01 * sin(i * 3.1416 / 180) - 0.3, 0.0f);
+	}
+	glEnd();
+
+}
+void dibujarTronco() {
+	glBegin(GL_TRIANGLE_STRIP);
+	glColor3f(0.7f, 0.5f, 0.2f);
+	glVertex3f(-0.8f, -0.52f, 0.0f);
+	glVertex3f(-0.6f, -0.52f, 0.0f);
+	glVertex3f(-0.6f, -0.08f, 0.0f);
+	glVertex3f(-0.8f, -0.08f, 0.0f);
+	glVertex3f(-0.8f, -0.52f, 0.0f);
+
+	glEnd();
+
+}
+void dibujarHojas() {
+	glBegin(GL_POLYGON);
+	glColor3f(0.1f, 1.0f, 0.1f);
+	for (double i = 0; i < 360; i += 0.5) {
+		glVertex3f(0.25 * cos(i * 3.1416 / 180) - 0.7, 0.15 * sin(i * 3.1416 / 180) - 0.0, 0.0f);
+	}
+	for (double i = 0; i < 360; i += 0.5) {
+		glVertex3f(0.25 * cos(i * 3.1416 / 180) - 0.7, 0.15 * sin(i * 3.1416 / 180) + 0.2, 0.0f);
+	}
+	for (double i = 0; i < 360; i += 0.5) {
+		glVertex3f(0.25 * cos(i * 3.1416 / 180) - 0.7, 0.15 * sin(i * 3.1416 / 180) + 0.4, 0.0f);
+	}
+	glEnd();
+}
+void dibujarRayosSol() {
+	glBegin(GL_LINE_STRIP);
+	glColor3f(1.0f, 0.96f, 0.21f);
+	glVertex3f(-0.7f, 1.0f, 0.0f);
+	glVertex3f(-0.6f,1.0f,0.0f);
+	glVertex3f(-0.7f, 0.9f, 0.0f);
+	glVertex3f(-0.7f, 0.8f, 0.0f);
+	glVertex3f(-0.5f, 0.6f, 0.0f);
 
 
 	glEnd();
@@ -111,13 +151,18 @@ void dibujarPuerta() {
 void dibujar() {
 	dibujarTecho();
 	dibujarCesped();
+	dibujarTronco();
+	dibujarHojas();
 	dibujarCasa();
 	dibujarMarcoventana();
 	dibujaVentana();
 
 	dibujarPuerta();
+	dibujarPomo();
 	dibujarMarcoVentanaInt();
 	dibujarSol();
+	dibujarRayosSol();
+
 }
 
 int main()
